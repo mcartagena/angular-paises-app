643669426f14fb2ed0e651cad441d34227d8e959
Marcelo Cartagena <120571996+marcecarta@users.noreply.github.com>
2023-01-25 12:15:59 -0300

    working with DebounceTime

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -27,4 +27,9 @@ export class PorPaisComponent {
       }
     );
   }
+  sugerencias(termino: string) {
+    this.hayError = false;
+    console.log(termino);
+    // TODO: crear sugerencias
+  }
 }
